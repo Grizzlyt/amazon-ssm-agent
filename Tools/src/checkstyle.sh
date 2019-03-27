4f2bb8444c26e57ef97aa8a220408bd8223e4f59
Valiantsin Malyshka <vmalyshka@cloudpassage.com>
2019-03-27 16:12:46 +0300

    Checking goimport

--- Tools/src/checkstyle.sh
+++ Tools/src/checkstyle.sh
@@ -5,12 +5,11 @@ echo "Run checkstyle script"
 echo "Run 'gofmt'"
 unformatted=$(gofmt -l `pwd`/agent/)
 if [[ -n $unformatted ]]; then
-	echo >&2 "Error: Found files not formatted by gofmt"
+	echo >&2 "Found files not formatted by gofmt"
 	for fi in $unformatted; do
-		echo >&2 $fi
+		echo "Running 'gofmt -w' for $fi file"
+		gofmt -w $fi
 	done
-	echo "Please run 'gofmt -w' for files listed."
-	exit 1
 fi
 
 # run goimports
@@ -20,12 +19,11 @@ GOPATH=`pwd`/Tools go get golang.org/x/tools/cmd/goimports
 echo "Run 'goimports'"
 unformatted=$(Tools/bin/goimports -l `pwd`/agent/)
 if [[ -n $unformatted ]]; then
-	echo >&2 "Error: Found files not formatted by goimports"
+	echo >&2 "Found files not formatted by goimports"
 	for f in $unformatted; do
-		echo >&2 $f
+		"Running 'goimports -w' for $f file"
+		./Tools/bin/goimports -w $f
 	done
-	echo "Please run 'goimports -w' for files listed."
-	exit 1
 fi
 
 echo "Run 'go vet'"
